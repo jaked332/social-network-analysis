1dab4a6070881b46c78b47594e3c69f9cd606684
WaltzAxlTuzon <waltzaxl@hawaii.edu>
2024-08-04 15:28:09 -1000

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,17 +1,27 @@
 # Import necessary packages
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-import matplotlib.pyplot as plt
-from data import generate_fake_users
+import random
+#from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+#import matplotlib.pyplot as plt
+import secrets
+import names
 
-stopWords = set(STOPWORDS)
+#stopWords = set(STOPWORDS)
 
-print(stopWords)
+#print(stopWords)
 
 # Function used to make a list of dictionaries of user's as key and their list of attributes as value (age, gender, region).
 # [{“userName1” : [attribute1, attribute2, attribute3]}, ...]
-# Let's say we can only allow people who are 18 and older
+def createUsers(userNum = 3, minAge = 5, maxAge = 100):
+    createdUsers = []
 
-#Function used to make a list of dictionaries of user's name as key and a list of string words as value.
+    for users in userNum:
+        age = random.randint(minAge, maxAge)
+        gender = random.choice(['Male', 'Female', 'Non-binary'])
+        userName = names.get_full_name(gender)
+        region = random.choice(["Africa", "Asia", "Central America", "Europe", "Middle East", "North America", "Pacific", "South America"])
+
+        currentUser = {}
+# Function used to make a list of dictionaries of user's name as key and a list of string words as value.
 
 # Function used to make of list of dictionaries of words as key and frequency as their integer values another dictionary 
 # which has a userName as key and list of people who have used that word as value :
