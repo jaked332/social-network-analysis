3c4a6589874424eff0466a51b1f6a52a8369dfd6
andy3-git <andy3@hawaii.edu>
2024-08-04 20:05:26 -1000

    Create task_1.py

---
+++ task_1.py
@@ -0,0 +1,11 @@
+from typing import List
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+from data import (
+    generate_fake_users,
+    generate_fake_posts,
+    generate_fake_comments,
+    generate_fake_views)
