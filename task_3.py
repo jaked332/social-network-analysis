2a8f0b00b4a044664db34a5acebbf253dbd60344
WaltzAxlTuzon <waltzaxl@hawaii.edu>
2024-08-04 16:40:55 -1000

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,18 +1,16 @@
 # Import necessary packages
 import random
-#from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-#import matplotlib.pyplot as plt
-import secrets
+import matplotlib.pyplot as plt
 import names
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
-#stopWords = set(STOPWORDS)
-
-#print(stopWords)
+stopWords = set(STOPWORDS)
 
 # Function used to make a list of dictionaries of user's as key and their list of attributes as value (age, gender, region).
 # [{“userName1” : [attribute1, attribute2, attribute3]}, ...]
-def createUsers(userNum = 3, minAge = 5, maxAge = 100):
+def generateUsers(userNum = 3, minAge = 5, maxAge = 100):
     createdUsers = []
+    counter = 0
 
     for users in userNum:
         age = random.randint(minAge, maxAge)
@@ -20,14 +18,28 @@ def createUsers(userNum = 3, minAge = 5, maxAge = 100):
         userName = names.get_full_name(gender)
         region = random.choice(["Africa", "Asia", "Central America", "Europe", "Middle East", "North America", "Pacific", "South America"])
 
-        currentUser = {userName : [age, gender, region]}
-        createdUsers.append(currentUser)
+        # Conditionals, or user attributes, that needs to be met so that words can be included in the wordcloud.
+        if (age >= criteria['age']) and (gender == criteria['gender']) and (region == criteria['region']):
+            currentUser = {userName : [age, gender, region]}
+            createdUsers.append(currentUser)
+            counter += 1
     
-    print(f"Generated {userNum} profiles.")
+    print(f"Generated {counter} profiles out of {userNum} generated users.")
     return createdUsers
-
-# Function used to make a list of dictionaries of user's name as key and a list of string words as value.
 
 # Function used to make of list of dictionaries of words as key and frequency as their integer values another dictionary 
 # which has a userName as key and list of people who have used that word as value :
 #  [{“word1” : frequency}, ...]
+def generateUserPosts(users, maxNumPost = 3):
+    
+    for user in (len(users) - 1):
+
+# Criteria that must be met so that it is included in the wordcloud.
+criteria = {
+            'age' : 18,
+            'gender' : 'Male',
+            'region' : 'North America'
+            }
+
+# Generate new users with their attributes
+newUsers = generateUsers(5, 10, 65)
